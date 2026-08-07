06b45e5d82f4f80ed5cf77337ddc92608acc93f8
Streel <63997593+Streels@users.noreply.github.com>
2026-08-07 08:30:43 +0300

    Bust dashboard module cache

--- dashboard/js/app.js
+++ dashboard/js/app.js
@@ -2,7 +2,7 @@ import { ActivityCalendar } from "./components/activity-calendar.js";
 import { OverviewDashboard } from "./components/overview-dashboard.js";
 import { QuoteRotator } from "./components/quote-rotator.js";
 import { RecompositionChart } from "./components/recomposition-chart.js";
-import { DataService } from "./data-service.js";
+import { DataService } from "./data-service.js?v=20260807";
 
 function initTabs() {
   const tabs = document.querySelectorAll("[data-tab]");
