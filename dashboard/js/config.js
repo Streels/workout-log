dc56703c56fc1f41ef2305575e278c597350d0b6
Streel <63997593+Streels@users.noreply.github.com>
2026-09-09 21:13:10 +0300

    Add activities manifest URL to dashboard config

--- dashboard/js/config.js
+++ dashboard/js/config.js
@@ -1,6 +1,7 @@
 export const DATA_URLS = Object.freeze({
   workouts: new URL("../../data/workouts.json", import.meta.url),
   activities: new URL("../../data/activity.json", import.meta.url),
+  activityManifest: new URL("../../data/activities/index.json", import.meta.url),
   measurements: new URL("../../data/body-metrics.json", import.meta.url),
   quotes: new URL("../../data/quotes.json", import.meta.url),
 });
